75558875ee811d5ed24e48d6ac9e62591b29bd2e
PhilV1 <werpub.phil@gmx.de>
2024-04-03 16:20:00 +0200

    Refactor Projects component to use dynamic project information

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,26 @@
 import ProjectCard from '../utilities/ProjectCard'
 
 function Projects() {
+  const projectInfo = [
+    {
+      link: 'https://task-planner-five.vercel.app/',
+      image: '/images/TaskPlanner.JPG',
+      description:
+        'ToDo List with random Taskgenerator. Created with React and TailwindCSS.',
+    },
+    {
+      link: 'https://dice-game-one-silk.vercel.app/',
+      image: '/images/DiceGame.JPG',
+      description: 'DiceGame with vanilla HTML,CSS and Javascript.',
+    },
+    {
+      link: 'https://portfolio-website-omega-sepia-99.vercel.app/',
+      image: '/images/JsPortfolio.JPG',
+      description:
+        'Portfolio with vanilla HTML, CSS and a bit JS. Includes some of my older Projects',
+    },
+  ]
+
   return (
     <div
       id="projects"
@@ -8,27 +28,11 @@ function Projects() {
     >
       <h2 className="text-3xl font-bold pb-4">Recent Projects</h2>
       <div className="flex justify-center flex-wrap gap-6 text-white">
-        <ProjectCard
-          link="https://task-planner-five.vercel.app/"
-          image="/images/TaskPlanner.JPG"
-        >
-          ToDo List with random Taskgenerator. Created with React and
-          TailwindCSS.
-        </ProjectCard>
-        d
-        <ProjectCard
-          link="https://dice-game-one-silk.vercel.app/"
-          image="/images/DiceGame.JPG"
-        >
-          DiceGame with vanilla HTML,CSS and Javascript .
-        </ProjectCard>
-        <ProjectCard
-          link="https://portfolio-website-omega-sepia-99.vercel.app/"
-          image="/images/JsPortfolio.JPG"
-        >
-          Portfolio with vanilla HTML, CSS and a bit JS. Includes some of my
-          older Projects
-        </ProjectCard>
+        {projectInfo.map(({ link, image, description }) => (
+          <ProjectCard link={link} image={image}>
+            {description}
+          </ProjectCard>
+        ))}
       </div>
     </div>
   )
