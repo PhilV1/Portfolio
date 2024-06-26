621b77ff4739bf32f08a799e036cb628ca4146a3
PhilV1 <werpub.phil@gmx.de>
2024-06-26 10:38:46 +0200

    Refactor Projects component to use dynamic project information

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,26 +1,7 @@
 import ProjectCard from '../utilities/ProjectCard'
+import { projectInfo } from '../data/projectData'
 
 function Projects() {
-  const projectInfo = [
-    {
-      link: 'https://task-planner-five.vercel.app/',
-      image: '/images/TaskPlanner.JPG',
-      description:
-        'ToDo List with random Taskgenerator. Created with React and TailwindCSS.',
-    },
-    {
-      link: 'https://dice-game-one-silk.vercel.app/',
-      image: '/images/DiceGame.JPG',
-      description: 'DiceGame with vanilla HTML,CSS and Javascript.',
-    },
-    {
-      link: 'https://portfolio-website-omega-sepia-99.vercel.app/',
-      image: '/images/JsPortfolio.JPG',
-      description:
-        'Portfolio with vanilla HTML, CSS and a bit JS. Includes some of my older Projects',
-    },
-  ]
-
   return (
     <div
       id="projects"
---
+++ src/data/projectData.js
@@ -0,0 +1,19 @@
+export const projectInfo = [
+  {
+    link: 'https://task-planner-five.vercel.app/',
+    image: '/images/TaskPlanner.JPG',
+    description:
+      'ToDo List with random Taskgenerator. Created with React and TailwindCSS.',
+  },
+  {
+    link: 'https://dice-game-one-silk.vercel.app/',
+    image: '/images/DiceGame.JPG',
+    description: 'DiceGame with vanilla HTML,CSS and Javascript.',
+  },
+  {
+    link: 'https://portfolio-website-omega-sepia-99.vercel.app/',
+    image: '/images/JsPortfolio.JPG',
+    description:
+      'Portfolio with vanilla HTML, CSS and a bit JS. Includes some of my older Projects',
+  },
+]
